6e057d5832d65ad08576cf4743698afbc062929e
Panos Moisiadis <panosmoisiadis@pm.me>
2023-07-16 21:58:46 -0700

    Remove redundant access modifier in interface

--- src/main/java/org/moisiadis/Graph.java
+++ src/main/java/org/moisiadis/Graph.java
@@ -15,59 +15,59 @@ public interface Graph<T> {
 	 * Adds a vertex to the graph.
 	 * @param data the data of the vertex to be added.
 	 */
-	public void addVertex(T data);
+	void addVertex(T data);
 
 	/**
 	 * Adds an edge between two vertices.
 	 * @param data1 the data of the first vertex.
 	 * @param data2 the data of the second vertex.
 	 */
-	public void addEdge(T data1, T data2);
+	void addEdge(T data1, T data2);
 
 	/**
 	 * Removes a vertex from the graph.
 	 * @param data the data of the vertex to be removed.
 	 */
-	public void removeVertex(T data);
+	void removeVertex(T data);
 
 	/**
 	 * Removes an edge between two vertices.
 	 * @param data1	the data of the first vertex.
 	 * @param data2 the data of the second vertex.
 	 */
-	public void removeEdge(T data1, T data2);
+	void removeEdge(T data1, T data2);
 
 	/**
 	 * Returns the adjacent vertices of a vertex.
 	 * @param data the data of the vertex.
 	 * @return the adjacent vertices of the vertex.
 	 */
-	public Optional<List<T>> getAdjVertices(T data);
+	Optional<List<T>> getAdjVertices(T data);
 
 	/**
 	 * Returns all the vertices of the graph.
 	 * @return all the vertices of the graph.
 	 */
-	public Set<T> getVertices();
+	Set<T> getVertices();
 
 	/**
 	 * Returns the number of vertices in the graph.
 	 * @return the number of vertices in the graph.
 	 */
-	public int getVertexCount();
+	int getVertexCount();
 
 	/**
 	 * Returns the number of edges in the graph.
 	 * @return the number of edges in the graph.
 	 */
-	public int getEdgeCount();
+	int getEdgeCount();
 
 	/**
 	 * Returns true if the graph contains the vertex.
 	 * @param data the data of the vertex.
 	 * @return true if the graph contains the vertex.
 	 */
-	public boolean hasVertex(T data);
+	boolean hasVertex(T data);
 
 	/**
 	 * Returns true if the graph contains an edge between the two vertices.
@@ -75,27 +75,27 @@ public interface Graph<T> {
 	 * @param data2 the data of the second vertex.
 	 * @return true if the graph contains an edge between the two vertices.
 	 */
-	public boolean hasEdge(T data1, T data2);
+	boolean hasEdge(T data1, T data2);
 
 	/**
 	 * Returns the vertices of the graph in depth-first order.
 	 * @param data the data of the vertex to start the traversal from.
 	 * @return the vertices of the graph in depth-first order.
 	 */
-	public Set<T> dfs(T data);
+	Set<T> dfs(T data);
 
 	/**
 	 * Returns the vertices of the graph in breadth-first order.
 	 * @param data the data of the vertex to start the traversal from.
 	 * @return the vertices of the graph in breadth-first order.
 	 */
-	public Set<T> bfs(T data);
+	Set<T> bfs(T data);
 
 	/**
 	 * Returns true if the graph is cyclic. Redundant for non-directed graphs.
 	 * @return true if the graph is cyclic.
 	 */
-	public boolean isCyclic();
+	boolean isCyclic();
 
 	/**
 	 * Returns the vertices of the graph in topological order.
@@ -103,5 +103,5 @@ public interface Graph<T> {
 	 * @param data2 the data of the vertex to end the traversal at.
 	 * @return the vertices of the graph in topological order.
 	 */
-	public Set<T> shortestPath(T data1, T data2);
+	Set<T> shortestPath(T data1, T data2);
 }
